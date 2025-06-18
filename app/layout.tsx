ceb14eb580c9c955504d0cfd943af759f6b6e11c
mounirclicksalesmedia <your.email@example.com>
2025-06-18 20:01:46 +0400

    update update pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,14 +65,14 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1968993897242014');
+              fbq('init', '1372446060652479');
               fbq('track', 'PageView');
             `,
           }}
         />
         <noscript>
           <img height="1" width="1" style={{display: 'none'}}
-            src="https://www.facebook.com/tr?id=1968993897242014&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1372446060652479&ev=PageView&noscript=1"
           />
         </noscript>
         {/* End Meta Pixel Code */}
